f88da3ef21ddf3edb325e5f93c29c7b22a1e5e8c
Crowdbotics <team@crowdbotics.com>
2023-06-02 21:27:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/timeslotter_41201/MainActivity.java
@@ -0,0 +1,15 @@
+package com.timeslotter_41201;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "timeslotter_41201";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "timeslotter-41201",
+  url: "https://timeslotter-41201.botics.co",
+  api: "https://timeslotter-41201.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
